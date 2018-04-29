c47460c82ee48851431c8d69f241fbc3d79c8e7d
VZ <vitaly.zibulski@gmail.com>
2018-04-29 15:39:10 +0300

    fix model, create channel

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\User;
+use App\Models\User;
+use App\Models\Channel;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+
 
 class RegisterController extends Controller
 {
@@ -50,6 +52,7 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => 'required|string|max:255',
+            'channel_name' => 'required|max:255|unique:channels,name',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
         ]);
@@ -59,14 +62,25 @@ class RegisterController extends Controller
      * Create a new user instance after a valid registration.
      *
      * @param  array  $data
-     * @return \App\User
+     * @return \App\Models\User
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+
+
+
+		$user->channel()->create([
+        	'name' => $data['channel_name'],
+			'slug' => uniqid(true),
+		]);
+
+		return $user;
+
+
     }
 }
--- database/migrations/2018_04_29_114656_create_channels_table.php
+++ database/migrations/2018_04_29_114656_create_channels_table.php
@@ -18,7 +18,7 @@ class CreateChannelsTable extends Migration
             $table->string('name');
             $table->string('slug')->unique();
             $table->text('description')->nullable();
-            $table->string('image_filename')->nullable;
+            $table->string('image_filename')->nullable();
             $table->timestamps();
 
 			$table->integer('user_id')->unsigned();
