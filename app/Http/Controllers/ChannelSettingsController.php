9ab4493a758854af6d4c61b4312436001fde23e2
VZ <vitaly.zibulski@gmail.com>
2018-04-30 11:52:21 +0300

    add policy

--- app/Http/Controllers/ChannelSettingsController.php
+++ app/Http/Controllers/ChannelSettingsController.php
@@ -8,13 +8,26 @@ use App\Http\Requests\ChannelUpdateRequest;
 
 class ChannelSettingsController extends Controller
 {
-    public function edit(Channel $channel){
+	/**Display edit form
+	 *
+	 * @param Channel $channel
+	 * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+	 * @throws \Illuminate\Auth\Access\AuthorizationException
+	 */
+	public function edit(Channel $channel){
 
+		$this->authorize('edit',$channel);
     	return view('channel.settings.edit',compact('channel'));
 
 	}
 
+	/**Update channel
+	 *
+	 * @param ChannelUpdateRequest $request
+	 * @param Channel $channel
+	 * @throws \Illuminate\Auth\Access\AuthorizationException
+	 */
 	public function update(ChannelUpdateRequest $request, Channel $channel){
-		dd('update');
+		$this->authorize('update',$channel);
 	}
 }
