31c13f4a4d03cf1b16734f9ace1c4ba3ee8fa263
VZ <vitaly.zibulski@gmail.com>
2018-04-30 10:57:06 +0300

    fix Request

--- app/Http/Requests/ChannelUpdateRequest.php
+++ app/Http/Requests/ChannelUpdateRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
 
 class ChannelUpdateRequest extends FormRequest
 {
@@ -23,9 +25,11 @@ class ChannelUpdateRequest extends FormRequest
      */
     public function rules()
     {
+    	$channelId= Auth::user()->channels->first()->id;
+
         return [
-            'name' => 'required|max:255|unique:channels,name',
-			'slug' => 'required|max:255|alpha_num|unique:channels,slug',
+            'name' => 'required|max:255|unique:channels,name,' . $channelId,
+			'slug' => 'required|max:255|alpha_num|unique:channels,slug,' . $channelId,
 			'description' => 'max:1000',
         ];
     }
